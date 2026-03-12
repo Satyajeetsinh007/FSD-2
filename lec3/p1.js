6e9d933a7e88160d3694d0c0562ff42a27484f5d
Satyajeetsinh007 <satyajadeja52@gmail.com>
2026-03-12 13:33:36 +0530

    Add files via upload

---
+++ lec3/p1.js
@@ -0,0 +1,21 @@
+// const message = function () {
+//     console.log("this msg is shown after 4 sec")
+// }
+// setTimeout(message, 4000);
+// const add = (a, b) => {
+//     let c = a + b;
+//     return c;
+// }
+// console.log(add(1, 2))
+// let a = 1;
+// let b = 2;
+// setInterval(() => {
+//     console.log(a + b);
+//     a = a + 1;
+//     b = b + 1;
+
+// }, 1000)
+
+// to increase font size an interval of 50ms and it should stop increasing when the font size reaches 250px
+
+
